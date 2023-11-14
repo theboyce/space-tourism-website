4eb40942786e05943cbe2a2dd1cc2da0113fbff1
theboyce <kwabenaadofo330@gmail.com>
2023-11-13 13:58:26 -1200

    fixed a small error on the mobile navbar

--- src/components/MobileNavbar.jsx
+++ src/components/MobileNavbar.jsx
@@ -9,7 +9,7 @@ const MobileNavbar = () => {
   const { toggleMenu, handleToggle } = useContext(NavContext);
 
   return (
-    <div className="h-[100vh] w-[70%] bg-green-500 md:hidden lg:hidden">
+    <div className="h-[100vh] w-[70%] bg-green-500 md:hidden lg:hidden absolute ">
       <div>
         <svg
           onClick={handleToggle}
@@ -17,7 +17,7 @@ const MobileNavbar = () => {
           width="20"
           height="21"
         >
-          <g fill="#D0D6F9" fill-rule="evenodd">
+          <g fill="#D0D6F9" fillRule="evenodd">
             <path d="M2.575.954l16.97 16.97-2.12 2.122L.455 3.076z" />
             <path d="M.454 17.925L17.424.955l2.122 2.12-16.97 16.97z" />
           </g>
@@ -25,22 +25,20 @@ const MobileNavbar = () => {
 
         {navElements.map((navElement, i) => {
           return (
-            <NavLink>
-              <NavLink
-                key={i}
-                to={`${navElement.link}`}
-                style={({ isActive }) =>
-                  isActive ? { borderBottom: '4px solid white' } : undefined
-                }
-                className={
-                  'text-white font-barlow text-[16px] tracking-widest flex items-center hover:border-b-4 hover:border-hover'
-                }
-              >
-                <span className="pr-[11px] font-bold md:hidden lg:block">
-                  {navElement.id}
-                </span>
-                {navElement.name}
-              </NavLink>
+            <NavLink
+              key={i}
+              to={`${navElement.link}`}
+              style={({ isActive }) =>
+                isActive ? { borderBottom: '4px solid white' } : undefined
+              }
+              className={
+                'text-white font-barlow text-[16px] tracking-widest flex items-center hover:border-b-4 hover:border-hover'
+              }
+            >
+              <span className="pr-[11px] font-bold md:hidden lg:block">
+                {navElement.id}
+              </span>
+              {navElement.name}
             </NavLink>
           );
         })}
